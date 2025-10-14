edaac5c4327be4073375cf00d9ea78aec72b6485
Nick Fahey <nfahey@live.com.au>
2025-10-14 16:20:45 +1100

    feat: Add company GET routes

--- app/routes/company_routes.py
+++ app/routes/company_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, jsonify
+from sqlalchemy import select
 
-from app.schemas import company_schema
+from app.schemas import company_schema, companies_schema
+from app.model import Company
 from app.db import db
 
 company_route_bp = Blueprint('company_routes', __name__, url_prefix='/company')
@@ -9,12 +11,13 @@ company_route_bp = Blueprint('company_routes', __name__, url_prefix='/company')
 @company_route_bp.route('/create_company', methods=['POST'])
 def add_company():
     '''Create a new shipping company
-    Expects:
-        Company_name: Name of the company
-        Country: Home country of the company
-        Email: Contact email address
-        Phone: Contact phone number
-        Address: Company street address
+    
+    Body data (JSON):
+        Company_name(str): Name of the company
+        Country(str): Home country of the company
+        Email(str): Contact email address
+        Phone(str): Contact phone number
+        Address(str): Company street address
     '''
     
     data = request.get_json()
@@ -25,3 +28,32 @@ def add_company():
     
     result = company_schema.dump(new_company)
     return jsonify(result), 201
+
+@company_route_bp.route('/<int:company_id>')
+def get_company(company_id:int):
+    '''Get a single company
+
+    Path Params:
+        company_id (int): ID of the company to retrieve
+    '''
+    company = db.session.get(Company, company_id)
+
+    if not company:
+        return {'message':f'No company with id {company_id}.'}, 404
+    
+    result = company_schema.dump(company)
+    return jsonify(result), 200
+
+@company_route_bp.route('/get_all_companies')
+def get_all_companies():
+    '''Get all companies
+
+    '''
+    stmt = select(Company)
+
+    #TODO: Add any query parameters
+
+    companies = db.session.scalars(stmt)
+
+    result = companies_schema.dump(companies)
+    return jsonify(result), 200
