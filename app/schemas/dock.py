f1a689b248cfea724757ef8415f040f6db79497a
Nick Fahey <nfahey@live.com.au>
2025-10-18 16:29:09 +1100

    feat: Add dock length validation

--- app/schemas/dock.py
+++ app/schemas/dock.py
@@ -21,6 +21,12 @@ class DockSchema(SQLAlchemyAutoSchema):
         validate.Regexp(r'^[a-zA-Z0-9]+$', error='Dock code must be alphanumeric and numbers only ([a-z], [A-Z], [0-9])')
     ])
 
+    dock_length = auto_field(validate=[
+        validate.Length(min=1, error='Dock length must be greater than 0.')
+    ])
+
+    #TODO: validation re. cargo types
+
 
 dock_schema = DockSchema()
 docks_schema = DockSchema(many=True)
