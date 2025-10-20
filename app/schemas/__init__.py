40550d16e04fc6d293ff6d087eab744b2d9e7d3e
Nick Fahey <nfahey@live.com.au>
2025-10-21 09:51:24 +1100

    refactor: Standardise file names

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,4 @@
-from .company import company_schema, companies_schema
-from .cargo import cargo_schema, cargos_schema
-from .ship import ship_schema, ships_schema
-from .dock import dock_schema, docks_schema, dock_cargos_schema
+from .company_schema import company_schema, companies_schema
+from .cargo_schema import cargo_schema, cargos_schema
+from .ship_schema import ship_schema, ships_schema
+from .dock_schema import dock_schema, docks_schema, dock_cargos_schema
