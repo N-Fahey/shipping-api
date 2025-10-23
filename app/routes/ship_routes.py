baa97f53718241bcf64c9656da9266add243b3fd
Nick Fahey <nfahey@live.com.au>
2025-10-24 10:59:24 +1100

    feat: Add optional query params to get ship route

--- app/routes/ship_routes.py
+++ app/routes/ship_routes.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 from app.schemas import ship_schema, ships_schema
 from app.model import Ship, Booking
 from app.db import db
-from app.errors import PathParamError, BodyError
+from app.errors import PathParamError, BodyError, QueryParamError
 
 ship_route_bp = Blueprint('ship_routes', __name__, url_prefix='/ship')
 
@@ -46,11 +46,31 @@ def get_ship(ship_id:int):
 @ship_route_bp.route('/GetAllShips')
 def get_all_ships():
     '''Get all ships
-
+    Query Params (All optional):
+        min_length (int): Retrieve ships longer than supplied length (in metres)
+        max_length (int): Retrieve ships shorter than supplied length (in metres)
+        cargo_type_id (int): Retrieve ships configured for cargo type with supplied ID
+        company_id (int): Retrieve ships owned by company with supplied ID
     '''
     stmt = select(Ship)
 
-    #TODO: Add  query parameters: per ship ID, per cargo type, min/max length?
+    min_length = request.args.get('min_length', type=int)
+    max_length = request.args.get('max_length', type=int)
+    cargo_type_id = request.args.get('cargo_type_id', type=int)
+    company_id = request.args.get('company_id', type=int)
+
+    # Min length filter
+    if min_length:
+        stmt = stmt.where(Ship.ship_length >= min_length)
+    #Max length filter
+    if max_length:
+        stmt = stmt.where(Ship.ship_length <= max_length)
+    # Cargo type IDs filter
+    if cargo_type_id:
+        stmt = stmt.where(Ship.cargo_type_id == cargo_type_id)
+    #Company filter
+    if company_id:
+        stmt = stmt.where(Ship.company_id == company_id)
 
     ships = db.session.scalars(stmt)
 
